54fcbe5f64a7fb7b09b2e8b2d2e0d22b9fb91cc0
Mitch Pierias <mitch@pierias.com>
2019-09-29 22:00:11 +1000

    Added mock data for contract tests

---
+++ test/mock-data/index.js
@@ -0,0 +1,41 @@
+exports.mockName = "Mitch Pierias";
+
+exports.mockLocation = "Brisbane, Australia";
+
+exports.mockOccupations = [
+  {
+    id: "",
+    role: "Software Engineer",
+    organization: "Dominos Pizza Enterprises Limited",
+    location: "Brisbane, Australia"
+  },
+  {
+    id: "",
+    role: "Full Stack Software Developer",
+    organization: "CUA",
+    location: "Brisbane, Australia"
+  },
+  {
+    id: "",
+    role: "Full Stack Software Developer",
+    organization: "Syndicate",
+    location: "Brisbane, Australia"
+  }
+];
+
+exports.mockOrganization = {
+  id: "",
+  name: "Resume",
+  image: "",
+  link: "https://mitch.pierias.com"
+};
+
+exports.mockSkills = [
+  "ReactJS",
+  "Solidity",
+  "Truffle",
+  "IPFS",
+  "Ethereum",
+  "Automation",
+  "Deployment"
+];
