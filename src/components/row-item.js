c25012fd151f1750fcc01828c1394ae75bec0386
Mitch Pierias <mitch@pierias.com>
2019-12-28 23:23:32 +1000

    Included decription

--- src/components/row-item.js
+++ src/components/row-item.js
@@ -1,23 +1,37 @@
 import React from 'react'
 import { Thumbnail } from "./thumbnail";
 import { Duration } from "./duration";
-import { RowItemDescription } from "./row-item-description";
 
-export const RowItem = (props) =>  (
+const DESCRIPTION_CLASS = 'description'
+
+export const RowItem = (props) => (
     <div className={'row-container'}>
         <Thumbnail src={props.thumb} />
         <div className={'content'}>
             <div className={'col'}>
-                <div className='cell'>
+                <div style={{ flex: 1 }}>
                     <h4 className={'title'}>{props.title}</h4>
-                    {props.subtitle && <div className='subtitle'>{props.subtitle}</div>}
+                    <div className='subtitle'>{props.subtitle}</div>
                 </div>
                 <div className={'detail'}>
-                    {props.location && <div className={'location'}>{props.location}</div>}
+                    {props.location && <div className={'location web'}>{props.location}</div>}
                     <Duration startTime={props.startTime} endTime={props.endTime} />
                 </div>
             </div>
-            {props.description && <RowItemDescription content={props.description}/>}
+            <RowItemDescription content={props.description} />
         </div>
     </div>
 )
+
+export const RowItemDescription = (props) => {
+
+    if ('string' === typeof props.content) {
+        return <div className={[DESCRIPTION_CLASS, 'web'].join(' ')}>{props.content}</div>
+    }
+
+    if (props.content instanceof Array) {
+        return <ul className={DESCRIPTION_CLASS}>{props.content.map((item, idx) => <li key={idx}>{item}</li>)}</ul>
+    }
+
+    return null
+}
