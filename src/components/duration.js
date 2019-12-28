a4ea142927827902e1526eda0bb0d6a6e8eeaa33
Mitch Pierias <mitch@pierias.com>
2019-12-28 23:22:46 +1000

    Added mobile date text

--- src/components/duration.js
+++ src/components/duration.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
-const getDurationText = (startDate, endDate) => {
+const getFullText = (startDate, endDate) => {
   const startDateText = getStartDateText(startDate, endDate)
   const endDateText = getEndDateText(endDate)
   if (startDateText && endDateText) {
@@ -17,16 +17,40 @@ const getStartDateText = (startTime, endDate = 0) => {
   if ('string' === typeof startTime) return startTime
   if ('number' === typeof startTime) startTime = new Date(startTime)
   if ('number' === typeof endDate) endDate = new Date(endDate)
-  return MONTHS[startTime.getMonth()] + (endDate.getTime() - startTime.getTime() > 10) ? ` 2018` : ''
+  return endDate.getFullYear() - startTime.getFullYear() > 0 ? `${MONTHS[startTime.getMonth()]} ${startTime.getFullYear()}` : MONTHS[startTime.getMonth()]
 }
 
 const getEndDateText = (endDate) => {
-  if (!endDate) return 'Current'
+  if (!endDate) return 'Present'
   if ('string' === typeof endDate) return endDate
   if ('number' === typeof endDate) endDate = new Date(endDate)
   return `${MONTHS[endDate.getMonth()]} ${endDate.getFullYear()}`
 }
 
+const getShortText = (startDate, endDate = 0) => {
+  const timespan = endDate - startDate
+
+  if (timespan <= 0 || !startDate) {
+    return 'Present'
+  }
+
+  if (timespan <= 3600000) {
+    const hours = Math.floor(timespan / 3600000)
+    return `${hours} hour${hours > 1 ? 's' : ''}`
+  }
+
+  if (timespan <= 86400000) {
+    const days = Math.floor(timespan / 86400000)
+    return `${days} day${days > 1 ? 's' : ''}`
+  }
+
+  const months = Math.floor(timespan / (86400000 * 30))
+  return `${months} month${months > 1 ? 's' : ''}`
+}
+
 export const Duration = (props) => {
-  return <div className={'occupation-timeline'}>{getDurationText(props.startTime, props.endTime)}</div>
+  return (<div className={'timeline'}>
+    <span className={'web'}>{getFullText(props.startTime, props.endTime)}</span>
+    <span className={'mobile'}>{getShortText(props.startTime, props.endTime)}</span>
+  </div>)
 }
